ec042d6daecde2b0a74d09320042b3c85b9eb1a0
brachtco <brachtdanny@gmail.com>
2023-04-09 12:59:02 -0600

    Created seeds folder Created seed.js schoolData.js userData.js files Input data to userData.js

---
+++ server/seeds/userData.js
@@ -0,0 +1,161 @@
+[
+    {
+        "name": "RobertD",
+        "Gender": "Male",
+        "Age": "17",
+        "Hometown": "Las Vegas, NV",
+        "Hobbies": "Piano, Baseball, Gaming",
+        "Smoke": "No",
+        "Drink": "No"
+    },
+
+    {
+        "name": "ScarlettJ",
+        "Gender": "Female",
+        "Age": "18",
+        "Hometown": "Dallas, TX",
+        "Hobbies": "Dance, Soccer, Crossword Puzzles",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "MarkR",
+        "Gender": "Male",
+        "Age": "19",
+        "Hometown": "Logan, UT",
+        "Hobbies": "Snowboarding, Animation, Bass Guitar",
+        "Smoke": "Yes",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "TomH",
+        "Gender": "Male",
+        "Age": "17",
+        "Hometown": "Brooklynm NY",
+        "Hobbies": "Biking, Gaming, Art",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "EvangelineL",
+        "Gender": "Female",
+        "Age": "20",
+        "Hometown": "Bend, OR",
+        "Hobbies": "Lacrosse, Singing, Camping",
+        "Smoke": "No",
+        "Drink": "No"
+    },
+
+    {
+        "name": "TomH",
+        "Gender": "Male",
+        "Age": "17",
+        "Hometown": "Encinitas, CA",
+        "Hobbies": "Surfing, Golf, Cooking",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "ChrisE",
+        "Gender": "Male",
+        "Age": "18",
+        "Hometown": "Raleigh, NC",
+        "Hobbies": "Piano, Filmaker, Biking",
+        "Smoke": "No",
+        "Drink": "No"
+    },
+
+    {
+        "name": "ZoeS",
+        "Gender": "Female",
+        "Age": "17",
+        "Hometown": "Tuscon, AZ",
+        "Hobbies": "Volleyball, Debate, Motocross",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "PaulR",
+        "Gender": "Male",
+        "Age": "20",
+        "Hometown": "Boise, ID",
+        "Hobbies": "Football, Fly Fishing, Math",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "GwyenthP",
+        "Gender": "Female",
+        "Age": "17",
+        "Hometown": "Austin, TX",
+        "Hobbies": "Wake Boarding, Soccer, Art",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "JeremyR",
+        "Gender": "Male",
+        "Age": "17",
+        "Hometown": "Vancouver, BC",
+        "Hobbies": "Basketball. Mountain Biking, Coding",
+        "Smoke": "No",
+        "Drink": "No"
+    },
+
+    {
+        "name": "BrieL",
+        "Gender": "Female",
+        "Age": "19",
+        "Hometown": "St. George, UT",
+        "Hobbies": "Golf, Rafting, Cello",
+        "Smoke": "No",
+        "Drink": "No"
+    },
+
+    {
+        "name": "ChrisH",
+        "Gender": "Male",
+        "Age": "17",
+        "Hometown": "Bar Harbor, ME",
+        "Hobbies": "Sailing, Lacrosse, Chemistry",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "SamuelLJ",
+        "Gender": "Male",
+        "Age": "18",
+        "Hometown": "Seattle, WA",
+        "Hobbies": "Photography, Football, Go Kart Racing",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "LupitaN",
+        "Gender": "Female",
+        "Age": "17",
+        "Hometown": "Orinda, CA",
+        "Hobbies": "Soccer, Clarinet, Graphic Desgin",
+        "Smoke": "No",
+        "Drink": "Yes"
+    },
+
+    {
+        "name": "Paul",
+        "Gender": "Male",
+        "Age": "19",
+        "Hometown": "Madison, WI",
+        "Hobbies": "Hockey, Canoeing, Chemistry",
+        "Smoke": "No",
+        "Drink": "No"
+    },
+]
