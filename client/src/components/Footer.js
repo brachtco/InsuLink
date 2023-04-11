7807757b3003288a3ebebd671e5d0dec13dfdc03
patrick-roof <115768554+patrick-roof@users.noreply.github.com>
2023-04-10 20:45:54 -0600

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+// import { useLocation, useNavigate } from 'react-router-dom';
 
 const Footer = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
+//     const location = useLocation();
+//     const navigate = useNavigate();
     return (
         <footer>
             <div>
