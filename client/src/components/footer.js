304faf3f24e710a95410b8c692bf1a84bd6f1590
patrick <roosterruf@gmail.com>
2023-04-10 20:32:55 -0600

    added header and footer, uncommented in app.js

---
+++ client/src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Footer = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    return (
+        <footer>
+            <div>
+                <h4>Code Riders™</h4>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
