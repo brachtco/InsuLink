cc592c6a7c02245ea96d9d2116b851e6bc04ed37
spencer lore <spencer.lore04@gmail.com>
2023-04-12 12:50:38 -0600

    tweaks and changes

--- client/src/pages/westernSchools.js
+++ client/src/pages/westernSchools.js
@@ -6,7 +6,10 @@ function westernSchool() {
     <div>
         {/* TODO: Link navbar buttons to associated pages */}
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">InsuLink</a>
+            <a class="navbar-brand" href="#">
+                <img src="\client\src\logo.svg" width="30" height="30" alt=""></img>
+                InsuLink
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
