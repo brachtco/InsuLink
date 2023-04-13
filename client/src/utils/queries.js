a165b4d86a7e5bd243cbb66d5b02ab992def419e
brachtco <brachtdanny@gmail.com>
2023-04-12 20:02:29 -0600

    Started mutatitions.js and queries.js files

---
+++ client/src/utils/queries.js
@@ -0,0 +1,30 @@
+import { gql } from '@appollo/client'
+
+export const QUERY_USERS = gql`
+    query getUsers {
+        users {
+            _id
+            userFirstName
+            userLastName
+            age
+            gender
+            bio
+            interests
+            hometown
+            email
+            password
+            school
+        }
+    }
+    `;
+
+    export const QUERY_SCHOOLS =gql`
+        query getSchools {
+            schools {
+                _id 
+                name
+                image
+                region
+            }
+        }
+        `
