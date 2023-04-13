037c582f3c9ff21fb528d44e1505b3ee529ba119
Joseph Hayes <joey.hayes.1996@gmail.com>
2023-04-12 20:57:22 -0600

    Updating client/queries per Danny's code

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,32 +1,30 @@
-import { gql } from '@apollo/client';
+import { gql } from '@apollo/client'
 
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-      }
+export const QUERY_USERS = gql`
+    query getUsers {
+        users {
+            _id
+            userFirstName
+            userLastName
+            age
+            gender
+            bio
+            interests
+            hometown
+            email
+            password
+            school
+        }
     }
-  }
-`;
+    `;
 
-export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
-      }
+export const QUERY_SCHOOLS = gql`
+    query getSchools {
+        schools {
+            _id
+            name
+            image
+            region
+        }
     }
-  }
-`;
+    `;
