0345959d93aa78cfb65dd20d0d1d1d0838704a3d
Peter Krysinski <pakrysin@gmail.com>
2023-04-14 11:08:31 -0600

    Adding styling to EasternSchools, WesternSchools, and LandingPage.  Formatting HomePage to be consistent with the others

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
 
     return (
         <div id="HomePage" style={{ height: "100vh", position: "relative" }}>
 
-            <div id="selectRegion" style={{ display: "flex", justifyContent: "center", marginBottom: "50px" }}>
+            <div id="selectRegion" style={{ display: "flex", justifyContent: "center", marginBottom: "50px", marginTop: "50px" }}>
                 <h1>Select Region...</h1>
             </div>
 
             <div id="westernEastern" style={{ display: "flex", justifyContent: "space-around" }}>
                 <Box sx={{ '& button': { m: 1 } }}>
-                    <Link to="/westernSchools" variant="contained" size="large" style={{ marginLeft: "30px", marginRight: "30px" }}>
-                        Western
+                    <Link to="/WesternSchools" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" component="span" size="large" style={{ marginLeft: "30px", marginRight: "30px" }}>
+                            Western
+                        </Button>
                     </Link>
-                    <Link to="/easternSchools" variant="contained" size="large" style={{ marginLeft: "30px", marginRight: "30px" }}>
-                        Eastern
-                    </Link>
+                    <Link to="/EasternSchools" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" component="span" size="large" style={{ marginLeft: "30px", marginRight: "30px" }}>
+                            Eastern
+                        </Button>
+                    </Link>                        
                 </Box>
             </div>
         </div>
