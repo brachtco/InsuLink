a443eee5abd559cfa66d37683c92debd27e55b19
Peter Krysinski <pakrysin@gmail.com>
2023-04-14 15:05:56 -0600

    Updating SelectedSchool.js to be informative cards with profile pictures instead of just buttons.  Also added Linkage between SelectedSchool and SelectedStudent so that when you choose a student, it takes you to their profile with their information displayed

--- client/src/pages/EasternSchools.js
+++ client/src/pages/EasternSchools.js
@@ -13,28 +13,28 @@ function EasternSchools() {
 
             <div id="easternSchools" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                 <div style={{ marginBottom: "20px", marginTop: "20px" }}>
-                    <Link to="/SelectedSchool" state={{collegeName:'Boston College'}} style={{ textDecoration: 'none' }}>
+                    <Link to="/SelectedSchool" state={{collegeName:'Boston College', collegePic:'/assets/images/BC.jpeg'}} style={{ textDecoration: 'none' }}>
                         <Button variant="contained" component="span" size="large">
                             Boston College
                         </Button>
                     </Link>
                 </div>
                 <div style={{ marginBottom: "20px", marginTop: "20px" }}>
-                    <Link to="/SelectedSchool" state={{collegeName:'Duke'}} style={{ textDecoration: 'none' }}>
+                    <Link to="/SelectedSchool" state={{collegeName:'Duke', collegePic:'/assets/images/Duke.png'}} style={{ textDecoration: 'none' }}>
                         <Button variant="contained" component="span" size="large">
                             Duke
                         </Button>
                     </Link>
                 </div>                
                 <div style={{ marginBottom: "20px", marginTop: "20px" }}>
-                    <Link to="/SelectedSchool" state={{collegeName:'Florida State University'}} style={{ textDecoration: 'none' }}>
+                    <Link to="/SelectedSchool" state={{collegeName:'Florida State University', collegePic:'/assets/images/FSU.png'}} style={{ textDecoration: 'none' }}>
                         <Button variant="contained" component="span" size="large">
                             Florida State University
                         </Button>
                     </Link>
                 </div>
                 <div style={{ marginBottom: "20px", marginTop: "20px" }}>
-                    <Link to="/SelectedSchool" state={{collegeName:'Harvard'}} style={{ textDecoration: 'none' }}>
+                    <Link to="/SelectedSchool" state={{collegeName:'Harvard', collegePic:'/assets/images/Harvard.jpeg'}} style={{ textDecoration: 'none' }}>
                         <Button variant="contained" component="span" size="large">
                             Harvard
                         </Button>
--- client/src/pages/WesternSchools.js
+++ client/src/pages/WesternSchools.js
@@ -13,28 +13,28 @@ function WesternSchools() {
 
             <div id="westernSchools" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                 <div style={{ marginBottom: "20px", marginTop: "20px" }}>
-                    <Link to="/SelectedSchool" state={{collegeName:'Arizona State University'}} style={{ textDecoration: 'none' }}>
+                    <Link to="/SelectedSchool" state={{collegeName:'Arizona State University', collegePic:'/assets/images/ASU.jpeg'}} style={{ textDecoration: 'none' }}>
                         <Button variant="contained" component="span" size="large" >
                             Arizona State University
                         </Button>
                     </Link>
                 </div>  
                 <div style={{ marginBottom: "20px", marginTop: "20px" }}>
-                    <Link to="/SelectedSchool" state={{collegeName:'San Diego State University'}} style={{ textDecoration: 'none' }}>
+                    <Link to="/SelectedSchool" state={{collegeName:'San Diego State University', collegePic:'/assets/images/SDSU.jpeg'}} style={{ textDecoration: 'none' }}>
                         <Button variant="contained" component="span" size="large">
                             San Diego State University
                         </Button>
                     </Link>
                 </div>        
                 <div style={{ marginBottom: "20px", marginTop: "20px" }}>
-                    <Link to="/SelectedSchool" state={{collegeName:'University of Oregon'}} style={{ textDecoration: 'none' }}>
+                    <Link to="/SelectedSchool" state={{collegeName:'University of Oregon', collegePic:'/assets/images/Oregon.jpeg'}} style={{ textDecoration: 'none' }}>
                         <Button variant="contained" component="span" size="large">
                             University of Oregon
                         </Button>
                     </Link>
                 </div>      
                 <div style={{ marginBottom: "20px", marginTop: "20px" }}>
-                    <Link to="/SelectedSchool" state={{collegeName:'University of Utah'}} style={{ textDecoration: 'none' }}>
+                    <Link to="/SelectedSchool" state={{collegeName:'University of Utah', collegePic:'/assets/images/UofU.png'}} style={{ textDecoration: 'none' }}>
                         <Button variant="contained" component="span" size="large">
                             University of Utah
                         </Button>
