dff1c27fb0893b20230535081ea797bea07a8ed7
spencer lore <spencer.lore04@gmail.com>
2023-04-13 19:42:00 -0600

    got east and west routes working and fixed issues

--- client/src/pages/easternSchools.js
+++ client/src/pages/easternSchools.js
@@ -4,16 +4,16 @@ function EasternSchool() {
 
     return(
     <div>
-         {/* TODO: Link navbar buttons to associated pages */}
+    
     
        
         <div id="EasternSchool">
-            <Card style={{ display: "flex", justifyContent: "center"}}>
+            <div style={{ display: "flex", justifyContent: "center"}}>
                 <button id="btn-1">Boston College</button>
                 <button id="btn-2">Florida State University</button>
                 <button id="btn-3">Harvard</button>
                <button id="btn-4">Duke</button>
-             </Card>
+             </div>
        
         </div>
     </div>
