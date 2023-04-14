2609e5be12acfb70f46af7fe45b890bbb171b39d
Peter Krysinski <pakrysin@gmail.com>
2023-04-14 11:08:31 -0600

    Adding styling to EasternSchools, WesternSchools, and LandingPage.  Formatting HomePage to be consistent with the others

--- client/src/pages/WesternSchools.js
+++ client/src/pages/WesternSchools.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 function WesternSchools() {
 
-    return(
-    <div>
+    return (
+        <div id="WesternSchools" style={{ height: "100vh", position: "relative" }}>
 
-      
-      
-        <div id="westernSchool">
-            <div style={{ display: "flex", justifyContent: "center"}}>
-             <button id="btn-1">Boston College</button>
-                <button id="btn-2">Florida State University</button>
-                <button id="btn-3">Harvard</button>
-               <button id="btn4">Duke</button>
-             </div>
-     
+            <div id="selectWesternSchool" style={{ display: "flex", justifyContent: "center", marginBottom: "50px", marginTop: "50px" }}>
+                <h1>Select Western School...</h1>
+            </div>
+
+            <div id="westernSchools" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <div style={{ marginBottom: "20px", marginTop: "20px" }}>
+                    <Link to="/SelectedSchool" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" component="span" size="large" style={{ marginLeft: "30px", marginRight: "30px" }}>
+                            University of Utah
+                        </Button>
+                    </Link>
+                </div>
+                <div style={{ marginBottom: "20px", marginTop: "20px" }}>
+                    <Link to="/SelectedSchool" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" component="span" size="large" style={{ marginLeft: "30px", marginRight: "30px" }}>
+                            San Diego State University
+                        </Button>
+                    </Link>
+                </div>
+                <div style={{ marginBottom: "20px", marginTop: "20px" }}>
+                    <Link to="/SelectedSchool" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" component="span" size="large" style={{ marginLeft: "30px", marginRight: "30px" }}>
+                            University of Oregon
+                        </Button>
+                    </Link>
+                </div>
+                <div style={{ marginBottom: "20px", marginTop: "20px" }}>
+                    <Link to="/SelectedSchool" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" component="span" size="large" style={{ marginLeft: "30px", marginRight: "30px" }}>
+                            Arizona State University
+                        </Button>
+                    </Link>
+                </div>
+            </div>
         </div>
-    </div>
     )
-
-}; 
+};
 
 export default WesternSchools;
